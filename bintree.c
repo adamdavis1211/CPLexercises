3b704773ea297d183f8dadc7f344ddd41ee0b838
db <adamndavis12@gmail.com>
2025-08-02 18:15:44 -0700

    self-referential structures

---
+++ bintree.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+#define MAXWORD 100
+
+struct tnode {		/* the tree node */
+	char *word;	/* points to the text */
+	int count;	/* number of occurrences */
+	struct tnode *left;	/* left child */
+	struct tnode *right;	/* right child */
+}
+
+struct tnode *addtree(struct node *, char *);
+void treeprint(struct tnode *);
+int getword(char *, int);
+
+/* word frequency count */
+int main() 
+{
+	struct tnode *root;
+	char word[MAXWORD];
+
+	root = NULL;
+	while (getword(word, MAXWORD) != EOF)
+		if (isalpha(word[0]))
+			root = addtree(root, word);
+	treeprint(root);
+	return 0;
+}
+
+struct tnode *talloc(void);
+char *strdup(char *);
+
+/* addtree:  add a node with w, at or below p */
+struct treenode *addtree(struct tnode *p, char *w)
+{
+	int cond;
+
+	if (p == NULL) {	/* a new word has arrived */
+		p = talloc();	/* make a new node */
+		p->word = strdup(w);	
+		p->count = 1;
+		p->left = p->right = NULL;
+	} else if ((cond = strcmp(w, p->word)) == 0)
+		p->count++;
+	else if (cond < 0)
+		p->left = addtree(p->left, w);
+	else
+		p->right = addtree(p->right, w);
+	return p;
+}
+
+/* treeprint:  in-order print of tree p */
+void treeprint(struct tnode *p)
+{
+	if (p != NULL) {
+		treeprint(p->left);
+		printf("%4d %s\n", p->count, p->word);
+		treeprint(p->right);
+	}
+}
+
+/* talloc:  make a tnode */
+struct tnode *talloc(void)
+{
+	return (struct tnode *) malloc(sizeof(struct tnode));
+}
+
+char *strdup(char *s)
+{
+	char *p;
+
+	p = (char *) malloc(strlen(s)+1);	/* +1 for '\0' */ 
+	if (p != NULL)
+		strcpy(p, s);
+	return p;
+}
--- keyword.c
+++ keyword.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+
 #define MAXWORD 100
+#define NKEYS (sizeof keytab / sizeof keytab[0])
+
+struct key {
+	char *word;
+	int count;
+} keytab[] = {
+	"auto", 0,
+	"break", 0,
+	"case", 0,
+	"char", 0,
+	"const", 0,
+	"continue", 0,
+	"default", 0,
+	/* ... */
+	"unsigned", 0,
+	"void", 0,
+	"volatile", 0,
+	"while", 0,
+};
 
 int getword(char *, int);
 struct key *binsearch(char *, struct key *, int);
